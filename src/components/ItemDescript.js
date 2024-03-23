300613bfac9248d585d51e9ac1d2f14d58ed5429
Kira Selucky <kiraashley24@gmail.com>
2024-03-23 14:04:22 -0600

    adding products and styling to productdetail page

--- src/components/ItemDescript.js
+++ src/components/ItemDescript.js
@@ -2,6 +2,15 @@ import React from 'react';
 import begonia from '../img/begonia.jpg';
 import celosia from '../img/celosia.jpg';
 import cosmos from '../img/cosmos.jpg';
+import dahlias from '../img/dahlias.jpg';
+import geraniums from '../img/geraniums.jpg';
+import impatients from '../img/impatients.jpeg';
+import marigolds from '../img/marigolds.jpg';
+import pansies from '../img/pansies.jpg';
+import petunias from '../img/petunias.jpeg';
+import snapdragons from '../img/snapdragons.jpg';
+import sunflowers from '../img/sunflowers.jpg';
+import zinnias from '../img/zinnias.jpeg';
 
 const ItemDescript = [
   {
@@ -22,6 +31,69 @@ const ItemDescript = [
     id: 3,
     name: "Cosmos",
     image: cosmos,
+    description: "Yet another beautiful flower.",
+    price: 5.99,
+  },
+  {
+    id: 4,
+    name: "Dahlias",
+    image: dahlias,
+    description: "Yet another beautiful flower.",
+    price: 5.99,
+  },
+  {
+    id: 5,
+    name: "Geraniums",
+    image: geraniums,
+    description: "Yet another beautiful flower.",
+    price: 5.99,
+  },
+  {
+    id: 6,
+    name: "Impatients",
+    image: impatients,
+    description: "Yet another beautiful flower.",
+    price: 5.99,
+  },
+  {
+    id: 7,
+    name: "Marigolds",
+    image: marigolds,
+    description: "Yet another beautiful flower.",
+    price: 5.99,
+  },
+  {
+    id: 8,
+    name: "Pansies",
+    image: pansies,
+    description: "Yet another beautiful flower.",
+    price: 5.99,
+  },
+  {
+    id: 9,
+    name: "Petunias",
+    image: petunias,
+    description: "Yet another beautiful flower.",
+    price: 5.99,
+  },
+  {
+    id: 10,
+    name: "Snapdragons",
+    image: snapdragons,
+    description: "Yet another beautiful flower.",
+    price: 5.99,
+  },
+  {
+    id: 11,
+    name: "Zinnias",
+    image: zinnias,
+    description: "Yet another beautiful flower.",
+    price: 5.99,
+  },
+  {
+    id: 12,
+    name: "Sunflowers",
+    image: sunflowers,
     description: "Yet another beautiful flower.",
     price: 5.99,
   },
--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -5,9 +5,9 @@ const ProductDetail = () => {
   return (
     <div className="products">
       {ItemDescript.map((product) => (
-        <div key={product.id} className="card">
+        <div key={product.id} className="product-card">
           <img src={product.image} alt={product.name} />
-          <div className="card-body">
+          <div className="product-card-body">
             <h2>{product.name}</h2>
             <p>{product.description}</p>
             <p>Price: ${product.price.toFixed(2)}</p>
