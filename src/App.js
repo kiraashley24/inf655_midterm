c55629565e3246cda268290a13d26568b648c25b
Kira Selucky <kiraashley24@gmail.com>
2024-03-25 17:08:56 -0600

    added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './pages/Header';
+import Footer from './pages/Footer';
 import Products from './components/Products';
 import Cart from './pages/Cart';
 import ItemDescript from './components/ItemDescript';
@@ -13,40 +14,19 @@ import { CartProvider } from './components/context/CartContext';
 
 //funcitonality being handled in APP ex.) search functionality
 const App = () => {
-  const [cart, setCart] = useState([]);
-
-  const addToCart = (product) => {
-    const updatedCart = [...cart, product];
-    setCart(updatedCart);
-  };
-
-  const removeFromCart = (productId) => {
-    const updatedCart = cart.filter((item) => item.id !== productId);
-    setCart(updatedCart);
-    console.log('Cart item removed:', productId); 
-  };
-
-  const calculateTotal = () => {
-    return cart.reduce((total, item) => total + item.price * item.quantity, 0);
-  };
-
-  const handleSubmit = (formData) => {
-    // Handle form submission logic here
-    console.log('Form submitted:', formData);
-  };
-
   return (
     <Router>
-      <CartProvider> {/* Wrap your entire application with CartProvider */}
+      <CartProvider>
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/products" element={<Products products={ItemDescript} addToCart={addToCart} />} />
+          <Route path="/products" element={<Products products={ItemDescript} />} />
           <Route path="/productdetail" element={<ProductDetail products={ItemDescript} />} />
-          <Route path="/cart" element={<Cart cart={cart} removeFromCart={removeFromCart} />} />
-          <Route path="/checkout" element={<Checkout cart={cart} setCart={setCart} totalPrice={calculateTotal()} handleSubmit={handleSubmit} />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
           <Route path="/thankyou" element={<ThankYou />} />
         </Routes>
+        <Footer />
       </CartProvider>
     </Router>
   );
---
+++ src/pages/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+      <div className="footer">
+        <h6>Feel free to contact us: kirafloral@gmail.com</h6>
+      </div>
+    );
+  }
